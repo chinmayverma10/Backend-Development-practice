795440f81caae43b4082e130cb5cca3cd12578c7
Chinmay <chinmay.codesworld@gmail.com>
2024-01-21 14:06:12 +0530

    node

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-console.log(process.argv);
+// console.log(process.argv);
 
 
-// const math = require("./math")
-// console.log(somevalue)
-// console.log(math.sum(2,4));
+// // const math = require("./math")
+// // console.log(somevalue)
+// // console.log(math.sum(2,4));
 
-const Fruitinfo = require("./fruits");
-let x = Fruitinfo[1].color;
-console.log(x);
+// const Fruitinfo = require("./fruits");
+// let x = Fruitinfo[1].color;
+// console.log(x);
+
+
+
+
+import { generate, count } from "random-words";
+
+console.log(generate());
