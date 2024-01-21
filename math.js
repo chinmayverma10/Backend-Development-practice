7e060fcdabde034666cf3f5b6b7f1014de92c2e8
Chinmay <chinmay.codesworld@gmail.com>
2024-01-21 12:46:04 +0530

    hello

---
+++ math.js
@@ -0,0 +1,16 @@
+// module.exports.sum = (a,b) => {
+//     return a+b;
+// }
+exports.multi = (a,b) => a*b;
+const g = 9.8;
+const pi = 3.14;
+
+// module.exports = 123;
+
+// module.exports = {
+//     sum: sum,
+//     multi: multi,
+//     g: g,
+//     pi: pi
+// };
+module.exports = obj;
---
+++ script.js
@@ -0,0 +1,10 @@
+console.log(process.argv);
+
+
+// const math = require("./math")
+// console.log(somevalue)
+// console.log(math.sum(2,4));
+
+const Fruitinfo = require("./fruits");
+let x = Fruitinfo[1].color;
+console.log(x);
